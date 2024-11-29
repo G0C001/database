c73ad1d72b2a976aad933f8dd8367226b9c688b7
G0C001 <gokulvbe1303@gmail.com>
2024-11-30 00:38:09 +0530

    first commit

--- excel/settings.py
+++ excel/settings.py
@@ -107,9 +107,6 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
